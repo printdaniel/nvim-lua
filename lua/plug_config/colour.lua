79e3ef40e5d17d90716a0f64907d2cb235ee5a6d
Daniel Suárez <dwsuarez98@gmail.com>
2022-12-01 10:39:31 -0300

    TSp udate

--- lua/plug_config/colour.lua
+++ lua/plug_config/colour.lua
@@ -1,13 +1,11 @@
 vim.o.background = "dark" -- or "light" for light mode
-vim.cmd([[colorscheme gruvbox]])
+-- vim.cmd([[colorscheme gruvbox]])
 -- vim.cmd([[colorscheme nightfox]])
 
 -- vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
--- vim.cmd[[colorscheme catppuccin]]
+vim.cmd[[colorscheme catppuccin]]
 -- require("catppuccin").setup()
 
 -- vim.cmd([[colorscheme material]])
 -- vim.g.material_style = "deep ocean"
-
-
 
--- lua/plug_config/lsp_config/lsp-config.lua
+++ lua/plug_config/lsp_config/lsp-config.lua
@@ -4,7 +4,7 @@
 
 local nvim_lsp = require('lspconfig')
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 local servers = {'tsserver', 'hls', 'cmake', 'html','cssls', 'rust_analyzer', 'sumneko_lua', 'bashls', 'ccls','clangd', 'marksman'}
 
